c3b9418a312670335fa6f93a4b83128502b4ddbc
Samuel <samuelou510@gmail.com>
2023-06-13 16:38:50 -0400

    looping redone

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+d// Code your solutions in this file
+function writeCards(arr) {
+  let thankYou = new Array(arr.length);
+
+  for (let i = 0; i < arr.length; i++) {
+    thankYou[i] = `Thank you, ${arr[i]}, for the wonderful surprise gift!`;
+  }
+  return thankYou;
+}
+
+function countDown(int) {
+  for (let i = int; i >= 0; i--) {
+    console.log(i);
+    }
+}
